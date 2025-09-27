796dfd74ade848221a02e76b4c58221f49da9ec0
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-09-27 09:53:52 -0500

    Created a model and view where user can create their notes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,7 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only" : True}} # tells django we want password only when creating user; but not be returning while giving info about user
         
-    #implementing a ethod that will be called when we are creating a user
+    #implementing a method that will be called when we are creating a user
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data) # **validated_data -> splits the keyword arguments and pass the data
         return user 
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}  # This doesn't allow to write the author name. Just author as the one who is writing the note
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
-# Create your views here.
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]    # can't call this route/view unless a valid JWT is passed
+    
+    # Getting an authenticated user
+    def get_queryset(self):
+        user = self.request.user
+        #return Note.objects.all() -> gives all the notes
+        return Note.objects.filter(author=user)       # gives notes of a specific user
+
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()             # list of all the object to be looking while creating so won't create already existing one
     serializer_class = UserSerializer         # tells what kind of data accept to create a user
-    permission_classes = [AllowAny]             # any one can call this view, even not authenticated
+    permission_classes = [AllowAny]           # any one can call this view, even not authenticated
     
     
     
