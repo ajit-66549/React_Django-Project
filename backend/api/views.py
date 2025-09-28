7b05b411b23d003f662b8451462c267e2f763254
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-09-27 23:51:20 -0500

    Create views for listing note, deleting note and added route for them

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,8 +13,21 @@ class NoteListCreate(generics.ListCreateAPIView):
     def get_queryset(self):
         user = self.request.user
         #return Note.objects.all() -> gives all the notes
-        return Note.objects.filter(author=user)       # gives notes of a specific user
-
+        return Note.objects.filter(author = user)       # gives notes of a specific user
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author = self.request.user)
+        else:
+            print(serializer.errors)
+            
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author = user)
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()             # list of all the object to be looking while creating so won't create already existing one
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/", include("api.urls")),
 ]
