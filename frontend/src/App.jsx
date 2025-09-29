b8dd0ab2c6b0984db67828af3d6e7ee34172a8a4
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-09-29 15:41:41 -0500

    Set up some pages and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
 import react from "react"
+import { BrowserRouter, Routes, Route, Navigation } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
+import Register from "./pages/Register"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
   
